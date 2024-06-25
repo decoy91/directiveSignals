c476098581afceb00934bc82986d9da868930ffb
Aleks <gajim91@gmail.com>
2024-06-25 13:06:31 -0600

    Formulario reactivo tradicional

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 
 @NgModule({
-  declarations: [  
+  declarations: [
     ProductPageComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
