6b7c54d63c1e1eeb37c2ff755ad3d02c0376c000
Aleks <gajim91@gmail.com>
2024-09-30 22:51:43 -0600

    Mutaciones, actualizar parte del estado

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,9 +7,43 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg'
+  })
+
   onFieldUpdate(field: string, value:string){
 
-    console.log({ field, value })
+    this.user.update(current => {
+      switch(field){
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number( value );
+          break;
+
+      }
+
+      return current;
+    })
 
   }
 }
